d6e7f83928aec59e8b1b82cb0ef9e9dd29ad560a
Vivek <vivekjha92@gmail.com>
2016-10-27 14:57:52 +0530

    Prototype 2 platform effects

--- Assets/Script/Proto v/ProtoPlatform.cs
+++ Assets/Script/Proto v/ProtoPlatform.cs
@@ -5,21 +5,36 @@ public class ProtoPlatform : MonoBehaviour
 {
     public enum PlatformType
     {
-        Normal,LeftSide,RightSide
+        Normal,LeftSide,RightSide,Blinking,Bouncing,Moving
     }
 
     public PlatformType myType;
     
     public float power;
 
+    void Start()
+    {
+        if(myType==PlatformType.Blinking)
+        {
+            StartCoroutine(Blinking());
+        }
+    }
+    
 	// Update is called once per frame
 	void Update ()
     {
-        if (ProtoVManager.Instance.gamePlaying && myType==PlatformType.Normal)
+        if (ProtoVManager.Instance.gamePlaying)
         {
-            //Debug.Log(transform.position);
-            transform.Translate(Vector3.up.normalized * 0.025f);
+            if(myType != PlatformType.LeftSide && myType != PlatformType.RightSide)
+            {
+                transform.Translate(Vector3.up.normalized * 0.025f);
+            }
 
+            if (myType == PlatformType.Moving)
+            {
+                transform.Translate(Vector3.right.normalized * 0.02f);
+            }
+            
             if (transform.position.y > 7)
                 Destroy(gameObject);
         }
@@ -27,15 +42,46 @@ public class ProtoPlatform : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("coll");
+        //Debug.Log("coll");
 
         if (other.gameObject.tag == "Player" && myType==PlatformType.LeftSide)
         {
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(other.gameObject.transform.right * power);
+            if(Time.timeSinceLevelLoad > 90)
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(other.gameObject.transform.right * (power+50));
+            else
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(other.gameObject.transform.right * power);
         }
         else if(other.gameObject.tag == "Player" && myType == PlatformType.RightSide)
         {
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(-other.gameObject.transform.right * power);
+            if (Time.timeSinceLevelLoad > 90)
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(-other.gameObject.transform.right * (power+50));
+            else
+                other.gameObject.GetComponent<Rigidbody2D>().AddForce(-other.gameObject.transform.right * power);
+        }
+        else if (other.gameObject.tag == "Player" && myType == PlatformType.Bouncing)
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(other.gameObject.transform.up * power);
+        }
+        else if(other.gameObject.tag == "Player")
+        {
+            if (other.gameObject.GetComponent<Rigidbody2D>().velocity.x < 1)
+                other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(other.gameObject.GetComponent<Rigidbody2D>().velocity.x + 1, 0, 0);
+            //Debug.Log(other.gameObject.GetComponent<Rigidbody2D>().velocity);
+        }
+
+    }
+
+    IEnumerator Blinking()
+    {
+        while (ProtoVManager.Instance.gamePlaying)
+        {
+            yield return new WaitForSeconds(0.5f);
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+            yield return new WaitForSeconds(0.5f);
+            GetComponent<BoxCollider2D>().enabled = true;
+            GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
         }
     }
+
 }
--- Assets/Script/Proto v/ProtoVManager.cs
+++ Assets/Script/Proto v/ProtoVManager.cs
@@ -42,7 +42,7 @@ public class ProtoVManager : MonoBehaviour
     {
         gamePlaying = true;
         StartCoroutine(GeneratePlatform());
-        StartCoroutine(GenerateCollectibles());
+        //StartCoroutine(GenerateCollectibles());
     }
 
     void Update()
@@ -53,6 +53,8 @@ public class ProtoVManager : MonoBehaviour
 
     public IEnumerator GeneratePlatform()
     {
+        int specialPlatformCount=0;
+
         while (gamePlaying)
         {
             GameObject tempP = Instantiate(platform);
@@ -60,7 +62,72 @@ public class ProtoVManager : MonoBehaviour
             tempP.transform.rotation = Quaternion.Euler(0, 0, 0);
             tempP.transform.SetParent(platformHolder.transform, false);
 
+            int r;
+
+            if (specialPlatformCount == 2)
+            {
+                r = Random.Range(7, 10);
+            }
+            else if (Time.timeSinceLevelLoad > 60)
+            {
+                r = Random.Range(1, 10);
+            }
+            else if(Time.timeSinceLevelLoad>40)
+            {
+                r = Random.Range(3, 10);
+            }
+            else if(Time.timeSinceLevelLoad > 15)
+            {
+                r = Random.Range(5, 10);
+            }
+            else
+            {
+                r = Random.Range(7, 10);
+            }
+
+            
+            if (r == 5|| r == 6)
+            {
+                tempP.GetComponent<ProtoPlatform>().myType = ProtoPlatform.PlatformType.Bouncing;
+                tempP.GetComponent<ProtoPlatform>().power = 150;
+                tempP.SetActive(true);
+                specialPlatformCount++;
+            }
+            else if(r==3|| r == 4)
+            {
+                tempP.GetComponent<ProtoPlatform>().myType = ProtoPlatform.PlatformType.Blinking;
+                tempP.SetActive(true);
+                specialPlatformCount++;
+            }
+            else if(r==1|| r == 2)
+            {
+                tempP.GetComponent<ProtoPlatform>().myType = ProtoPlatform.PlatformType.Moving;
+                tempP.SetActive(true);
+                specialPlatformCount++;
+            }
+            else
+            {
+                tempP.GetComponent<ProtoPlatform>().myType = ProtoPlatform.PlatformType.Normal;
+                tempP.SetActive(true);
+                if(specialPlatformCount>0)
+                    specialPlatformCount--;
+            }
+            Debug.Log(specialPlatformCount);
+
+
+            if(Random.Range(0,10)<5)
+            {
+                GameObject tempP2 = Instantiate(collectibles);
+                tempP2.transform.position = new Vector3(Random.Range(-2.5f, 2.5f), -3.5f, 0);
+                tempP2.transform.rotation = Quaternion.Euler(0, 0, 0);
+                tempP2.transform.SetParent(tempP.transform, true);
+            }
+            
+
             yield return new WaitForSeconds(2f);
+
+            //if (Time.timeSinceLevelLoad > 10)
+            //    Camera.main.transform.rotation = Quaternion.Euler(0,0,180);
         }
     }
 
